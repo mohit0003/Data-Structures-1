a7962769eb9e66b517373355f8f36ed5637feecf
deepakmca17du <31646033+deepakmca17du@users.noreply.github.com>
2018-01-21 21:06:34 +0530

    Add files via upload

---
+++ reverse_elements.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+#include<stack>
+
+stack <int> myStack;
+int main()
+{
+	int n;
+	double item;
+	cout<<"\nNo of elements:\t";
+	cin>>n;
+	
+	cout<<"\nEnter the elements:\t";
+	for(int i=0;i<n;i++)
+	{
+		cin>>item;
+		myStack.push(item);
+	}
+	
+	cout<<"\nReversed Elements:\t";
+	while(!myStack.empty())
+	{
+		cout<<myStack.top();
+		myStack.pop();
+		
+	}
+	
+	getch();
+	return 0;
+}
